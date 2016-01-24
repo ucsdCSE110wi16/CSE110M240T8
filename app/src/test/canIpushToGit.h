9e7825d5f799a34d76e05d66862448d5968dee7f
danielreznikov <daniel.reznikov@gmail.com>
2016-01-24 11:15:48 -0800

    This is a test to see if my Andriod Studio can push to GIT

---
+++ app/src/test/canIpushToGit.h
@@ -0,0 +1,16 @@
+//
+// Created by Daniel Reznikov on 1/24/16.
+//
+#include <iostream>
+
+#ifndef PROJECTPINACOLODA_CANIPUSHTOGIT_H
+#define PROJECTPINACOLODA_CANIPUSHTOGIT_H
+
+
+class canIpushToGit {
+
+std::cout << "this is a test and only a test. remain in yo seats.";
+};
+
+
+#endif //PROJECTPINACOLODA_CANIPUSHTOGIT_H

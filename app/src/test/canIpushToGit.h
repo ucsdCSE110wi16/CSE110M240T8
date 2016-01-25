68e5662607991a4f8d2c2ba4f8e9ff1d740b4b7f
Austin Stavenga <astaveng@eng.ucsd.edu>
2016-01-24 21:11:48 -0800

    Austin attempting to push

--- app/src/test/canIpushToGit.h
+++ app/src/test/canIpushToGit.h
@@ -11,7 +11,8 @@ class canIpushToGit {
 
 std::cout << "this is a test and only a test. remain in yo seats.";
     std::cout << "Darren can push too!";
-    std::cout << "Darren C here attempting to push!"
+    std::cout << "Darren C here attempting to push!";
+    std::cout << "Austin is also attempting to push";
 };
 
 

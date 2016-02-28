a2d2b108f3b8fb6d130050961ceaba862fc0ead0
Austin Stavenga <astaveng@eng.ucsd.edu>
2016-02-27 18:39:44 -0800

    Location field is now restricted to one line and auto-capitalization is working for name and location.

--- app/src/main/java/com/projectpinacolada/ucsd/projectpinacolada/Register.java
+++ app/src/main/java/com/projectpinacolada/ucsd/projectpinacolada/Register.java
@@ -3,6 +3,7 @@ package com.projectpinacolada.ucsd.projectpinacolada;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -51,6 +52,10 @@ public class Register extends AppCompatActivity {
         if(intent.hasExtra("password")){
             mPasswordField.setText(intent.getStringExtra("password"));
         }
+
+        mNameField.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+        mLocationField.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+
 
         Button registerButton = (Button) findViewById(R.id.registerButton);
         registerButton.setOnClickListener(new View.OnClickListener() {

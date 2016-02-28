0f4fa47a3bc88bad040f8115893454099039fd33
Austin Stavenga <astaveng@eng.ucsd.edu>
2016-02-27 22:30:42 -0800

    User profiles now include an image!

--- app/src/main/java/com/projectpinacolada/ucsd/projectpinacolada/UserProfile.java
+++ app/src/main/java/com/projectpinacolada/ucsd/projectpinacolada/UserProfile.java
@@ -3,13 +3,22 @@ package com.projectpinacolada.ucsd.projectpinacolada;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+
+import java.io.ByteArrayOutputStream;
 
 public class UserProfile extends AppCompatActivity {
 
@@ -19,6 +28,11 @@ public class UserProfile extends AppCompatActivity {
 
     Button editProfile;
     Button logout;
+    ImageButton profilePicture;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap imageBitmap;
+    private byte[] imageBytes;
+    private ParseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +44,13 @@ public class UserProfile extends AppCompatActivity {
         reviews = (TextView) findViewById(R.id.user_num_reviews);
         editProfile = (Button) findViewById(R.id.update_profile_button);
         logout = (Button) findViewById(R.id.logout_button);
+        profilePicture = (ImageButton) findViewById(R.id.profileImageButton);
+
+        user = ParseUser.getCurrentUser();
 
         setUserInfo();
+
+        getPicture();
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +63,13 @@ public class UserProfile extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startEditProfile();
+            }
+        });
+
+        profilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
             }
         });
     }
@@ -125,5 +151,65 @@ public class UserProfile extends AppCompatActivity {
 
         AlertDialog alert11 = builder1.create();
         alert11.show();
+    }
+
+    private void getPicture() {
+        ParseFile picture = (ParseFile)user.get("userImage");
+        if (picture != null) {
+            picture.getDataInBackground(new GetDataCallback() {
+                public void done(byte[] data, ParseException e) {
+                    if (e == null) {
+                        imageBytes = data;
+                        setImage();
+                    }
+                }
+            });
+        }
+    }
+
+    private void setImage() {
+        imageBitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+
+        if(imageBitmap != null){
+            profilePicture.setImageBitmap(Bitmap.createScaledBitmap(imageBitmap,250,250,false));
+        }
+    }
+
+    public void sendNewPicture() {
+
+        ParseFile file = new ParseFile("profileimage.png", imageBytes);
+        try {
+            file.save();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        user.put("userImage", file);
+        try {
+            user.save();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        setImage();
+
+    }
+
+    private void openCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            imageBytes = stream.toByteArray();
+            sendNewPicture();
+        }
     }
 }

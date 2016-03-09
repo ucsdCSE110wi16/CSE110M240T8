03fb65e18d882e099118375e985d5bebdb95ff31
danielreznikov <daniel.reznikov@gmail.com>
2016-03-09 12:04:19 -0800

    Implemented "no reviews yet" handling for brand new users on user profile page.

--- app/src/main/java/com/projectpinacolada/ucsd/projectpinacolada/UserProfile.java
+++ app/src/main/java/com/projectpinacolada/ucsd/projectpinacolada/UserProfile.java
@@ -2,6 +2,7 @@ package com.projectpinacolada.ucsd.projectpinacolada;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -11,11 +12,13 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.GetDataCallback;
 import com.parse.ParseException;
@@ -55,7 +58,7 @@ public class UserProfile extends AppCompatActivity {
         setTitle("My Profile");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        new RemoteDataTask().execute();
+
 
         name = (TextView) findViewById(R.id.user_name);
         location = (TextView) findViewById(R.id.user_location);
@@ -97,6 +100,25 @@ public class UserProfile extends AppCompatActivity {
         name.setText(currUser.getString("name"));
         location.setText(currUser.getString("location"));
         reviews.setText(Integer.toString(currUser.getInt("numReviews")));
+
+        // If user has reviews, pull them the down
+        if (currUser.getInt("numReviews") > 0)
+            new RemoteDataTask().execute();
+
+        // No Revews yet
+        else {
+            // Hide the listview
+            listview = (ListView) findViewById(R.id.listViewUserProfile);
+            listview.setVisibility(View.INVISIBLE);
+
+            // Display msg "No Reviews Yet"
+            Context context = getApplicationContext();
+            CharSequence text = "Hello toast!";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, R.string.toast_no_reviews_yet, duration);
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.show();
+        }
     }
 
     private void startEditProfile() {
